f22bd05e09f536b99434ecf60bf7ca58569337bf
DarioOrbita <104122781+DarioOrbita@users.noreply.github.com>
2022-08-19 22:39:36 -0500

    add finial fixes

--- seeds/post-seeds.js
+++ seeds/post-seeds.js
@@ -3,17 +3,17 @@ const { Post } = require('../models');
 const postdata = [
   {
     title: 'Why the fall of the Roman Empire was an inside job',
-    post_url: 'https://en.wikipedia.org/wiki/Sausage',
+    post_content: '11111111111111111111111111111111111111111111111111111111111',
     user_id: 1
   },
   {
     title: 'The cultural legacy of Morbius',
-    post_url: 'https://en.wikipedia.org/wiki/Sausage',
+    post_content: '2222222222222222222222222222222222222222222222',
     user_id: 2
   },
   {
     title: 'TOP 10 THINGS YOUR PROCTOLOGIST DOESNT WANT YOU TO KNOW (NOT CLICKBAIT)',
-    post_url: 'https://en.wikipedia.org/wiki/Sausage',
+    post_content: '333333333333333333333',
     user_id: 3
   }
 ];
